7816caba4282557b32ca902f27a1b1713e1c0040
David Lenaerts <david.lenaerts@gmail.com>
2016-02-12 12:58:19 +0100

    Update benchmark

--- benchmarks/RenderItemArray/script.js
+++ benchmarks/RenderItemArray/script.js
@@ -1,11 +1,36 @@
 var outputBox;
 var NUM_ITERATIONS = 10000;
+var NUM_ITEMS = 10000;
 var arrA, arrB, arrC, arrD;
+var linkedListHead = null;
+var renderItemHead = null;
+
+var LinkedListNode = function()
+{
+    this.renderItem = null;
+    this.next = null;
+};
+
+LinkedListNode.prototype = {};
 
 window.onload = function()
 {
     outputBox = document.getElementById("testOutput");
+    initItems();
 };
+
+function initItems()
+{
+    var linkedListItem = linkedListHead = new LinkedListNode();
+    var renderItem = renderItemHead = new HX.RenderItem();
+    for (var i = 0; i < NUM_ITEMS - 1; ++i) {
+        linkedListItem.next = new LinkedListNode();
+        linkedListItem = linkedListItem.next;
+
+        renderItem.next = new HX.RenderItem();
+        renderItem = renderItem.next;
+    }
+}
 
 function runBenchmark()
 {
@@ -20,6 +45,8 @@ function runBenchmark()
 
     arrD = [];
     benchmark("arr.length = 0, i++", runCaseD);
+
+    benchmark("Pooled linked list", runCaseE);
 }
 
 function benchmark(name, fnc)
@@ -34,33 +61,52 @@ function benchmark(name, fnc)
 
 function runCaseA()
 {
+    var renderItem = renderItemHead;
     arrA = [];
-    for (var i = 0; i < 10000; ++i) {
-        arrA.push(new HX.RenderItem());
+    for (var i = 0; i < NUM_ITEMS; ++i) {
+        arrA.push(renderItem);
+        renderItem = renderItem.next;
     }
 }
 
 function runCaseB()
 {
+    var renderItem = renderItemHead;
     arrB.length = 0;
-    for (var i = 0; i < 10000; ++i) {
-        arrB.push(new HX.RenderItem());
+    for (var i = 0; i < NUM_ITEMS; ++i) {
+        arrB.push(renderItem);
+        renderItem = renderItem.next;
     }
 }
 
 function runCaseC()
 {
-    arrC.length = 0;
-    for (var i = 0; i < 10000; ++i) {
-        arrC[i] = new HX.RenderItem();
+    var renderItem = renderItemHead;
+    arrC = [];
+    for (var i = 0; i < NUM_ITEMS; ++i) {
+        arrC[i] = renderItem;
+        renderItem = renderItem.next;
     }
 }
 
 function runCaseD()
 {
+    var renderItem = renderItemHead;
     arrD.length = 0;
-    for (var i = 0; i < 10000; ++i) {
-        arrD[i] = new HX.RenderItem();
+    for (var i = 0; i < NUM_ITEMS; ++i) {
+        arrD[i] = renderItem;
+        renderItem = renderItem.next;
+    }
+}
+
+function runCaseE()
+{
+    var renderItem = renderItemHead;
+    var node = linkedListHead;
+    for (var i = 0; i < NUM_ITEMS; ++i) {
+        node.renderItem = renderItem;
+        node = node.next;
+        renderItem = renderItem.next;
     }
 }
 

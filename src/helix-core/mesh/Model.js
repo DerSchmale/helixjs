ede551d4229bfe6b48357ea44755c86e9ca0a8ca
David Lenaerts <david.lenaerts@gmail.com>
2016-03-19 19:49:24 +0100

    Allow updating mesh data (untested)

--- src/helix-core/mesh/Model.js
+++ src/helix-core/mesh/Model.js
@@ -115,18 +115,9 @@ HX.Mesh = function (meshData, model)
     this._vertexStrides = [];
     this._indexBuffer = new HX.IndexBuffer();
 
-    for (var i = 0; i < meshData.numStreams; ++i) {
-        var buffer = new HX.VertexBuffer();
-        buffer.uploadData(meshData.getVertexData(i), meshData.vertexUsage);
-        this._vertexBuffers[i] = buffer;
-        this._vertexStrides[i] = meshData.getVertexStride(i);
-    }
-    this._indexBuffer.uploadData(meshData._indexData, meshData.indexUsage);
-
-    this._numIndices = meshData._indexData.length;
-
-    this._vertexAttributes = meshData._vertexAttributes;
     this._renderOrderHint = ++HX.Mesh.ID_COUNTER;
+
+    this.updateMeshData(meshData);
 };
 
 HX.Mesh.ID_COUNTER = 0;
@@ -134,6 +125,32 @@ HX.Mesh.ID_COUNTER = 0;
 
 HX.Mesh.prototype = {
     constructor: HX.Mesh,
+
+    updateMeshData: function(meshData)
+    {
+        var numStreams = meshData.numStreams;
+        var numVertexBuffers = this._vertexBuffers.length;
+
+        if (numStreams > numVertexBuffers) {
+            for (var i = numVertexBuffers; i < numStreams; ++i) {
+                this._vertexBuffers[i] = new HX.VertexBuffer();
+            }
+        }
+        else if (numStreams < numVertexBuffers) {
+            this._vertexBuffers.length = numStreams;
+            this._vertexStrides.length = numStreams;
+        }
+
+        for (var i = 0; i < numStreams; ++i) {
+            this._vertexBuffers[i].uploadData(meshData.getVertexData(i), meshData.vertexUsage);
+            this._vertexStrides[i] = meshData.getVertexStride(i);
+        }
+
+        this._numIndices = meshData._indexData.length;
+
+        this._indexBuffer.uploadData(meshData._indexData, meshData.indexUsage);
+        this._vertexAttributes = meshData._vertexAttributes;
+    },
 
     dispose: function ()
     {

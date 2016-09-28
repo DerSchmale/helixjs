c5402b1d4b7f25c610fc5fba740c85f82d6d6614
David Lenaerts <david.lenaerts@gmail.com>
2016-09-28 19:29:13 +0200

    Do not create normal depth pass if material needs normal depth itself!

--- src/helix-core/material/Material.js
+++ src/helix-core/material/Material.js
@@ -50,8 +50,10 @@ HX.Material.prototype =
         //else
         //    this._initDynamicLitPasses(geometryVertexShader, geometryFragment, lightingModel)
 
-        this._setPass(HX.MaterialPass.NORMAL_DEPTH_PASS, new HX.NormalDepthPass(this._geometryVertexShader, this._geometryFragmentShader));
         this._setPass(HX.MaterialPass.DIR_LIGHT_SHADOW_MAP_PASS, new HX.DirectionalShadowPass(this._geometryVertexShader, this._geometryFragmentShader));
+
+        if (!this._needsNormalDepth)
+            this._setPass(HX.MaterialPass.NORMAL_DEPTH_PASS, new HX.NormalDepthPass(this._geometryVertexShader, this._geometryFragmentShader));
 
         this._initialized = true;
         // TODO: init dynamic light passes
